390ca492a9fa26a103bc163c5793ece4e3711563
rezvai <Artem_pupkin_9791@mail.ru>
2023-04-01 13:20:01 +0300

    BASE-DIR for blog was done

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .models import Project
 def all_blogs(request):
-    return render(request, 'all_blogs.html')
+    projects = Project.objects.all()
+    return render(request, 'blog/all_blogs.html', {'projects': projects})
